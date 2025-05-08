5b061c7f2101c9bb0ca9fda31236e9eb37121625
angel <angel.canosa@alumnos.upm.es>
2025-05-08 13:21:09 +0200

    Arreglo cinemática

--- BrazoExcavador/Core/Inc/Cinematica.h
+++ BrazoExcavador/Core/Inc/Cinematica.h
@@ -9,23 +9,26 @@
 #define INC_CINEMATICA_H_
 
 typedef struct {
-    float x, y, z;
+    float x, y, z;          // Posición del efector final
+    float orientation;      // Orientación del efector (rotación propia - θ₄)
 } Position;
 
 typedef struct {
-    float theta1, theta2, theta3, theta4;
+    float theta1, theta2, theta3, theta4; // Ángulos de las articulaciones en grados
 } Angles;
 
 typedef struct {
-    float dx, dy, dz;
+    float dx, dy, dz;       // Velocidades lineales en cm/s
+    float dorientation;     // Velocidad angular del efector (en grados/s)
 } Velocity;
 
 typedef struct {
-    float dtheta1, dtheta2, dtheta3, dtheta4;
+    float dtheta1, dtheta2, dtheta3, dtheta4; // Velocidades articulares en grados/s
 } JointVelocity;
 
+// Prototipos
 Position forward_kinematics(Angles a);
-Angles inverse_kinematics(Position p);
+Angles inverse_kinematics(Position p, float desired_orientation);
 Velocity differential_kinematics(Angles a, JointVelocity dq);
 
 
--- BrazoExcavador/Core/Src/Cinematica.c
+++ BrazoExcavador/Core/Src/Cinematica.c
@@ -9,60 +9,71 @@
 #include "Cinematica.h"
 #include <math.h>
 
-#define L1 100.0f  // Aún por definir las longitudes de las extremidades
-#define L2 100.0f
-#define L3 100.0f
-#define L4 50.0f
+#define L1 14.0f //Aún no son definitivas
+#define L2 12.0f
+#define L3 12.0f
+
+#define DEG2RAD(a) ((a) * M_PI / 180.0f)
+#define RAD2DEG(a) ((a) * 180.0f / M_PI)
 
 Position forward_kinematics(Angles a) {
+    float t1 = DEG2RAD(a.theta1);
+    float t2 = DEG2RAD(a.theta2);
+    float t3 = DEG2RAD(a.theta3);
+
+    float r = L2 * cosf(t2) + L3 * cosf(t2 + t3);
+
     Position p;
-
-    float t1 = a.theta1 * M_PI / 180.0f;
-    float t2 = a.theta2 * M_PI / 180.0f;
-    float t3 = a.theta3 * M_PI / 180.0f;
-
-    float r = L2*cosf(t2) + L3*cosf(t2 + t3);
     p.x = cosf(t1) * r;
     p.y = sinf(t1) * r;
-    p.z = L1 + L2*sinf(t2) + L3*sinf(t2 + t3);
+    p.z = L1 + L2 * sinf(t2) + L3 * sinf(t2 + t3);
+    p.orientation = a.theta4;  // Orientación del efector (independiente)
 
     return p;
 }
 
-Angles inverse_kinematics(Position p) {
+Angles inverse_kinematics(Position p, float desired_orientation) {
     Angles a;
-    a.theta1 = atan2f(p.y, p.x) * 180.0f / M_PI;
+
+    // θ1: rotación base
+    a.theta1 = RAD2DEG(atan2f(p.y, p.x));
 
     float r = sqrtf(p.x * p.x + p.y * p.y);
     float z = p.z - L1;
 
+    // Ley del coseno para θ3
     float D = (r*r + z*z - L2*L2 - L3*L3) / (2 * L2 * L3);
-    a.theta3 = atan2f(sqrtf(1 - D*D), D) * 180.0f / M_PI;
+    if (D > 1.0f) D = 1.0f;
+    if (D < -1.0f) D = -1.0f;
 
+    float theta3_rad = atan2f(sqrtf(1 - D*D), D);
+    a.theta3 = RAD2DEG(theta3_rad);
+
+    // θ2: ángulo del primer brazo
     float phi = atan2f(z, r);
-    float beta = atan2f(L3*sinf(a.theta3*M_PI/180.0f), L2 + L3*cosf(a.theta3*M_PI/180.0f));
-    a.theta2 = (phi - beta) * 180.0f / M_PI;
+    float beta = atan2f(L3 * sinf(theta3_rad), L2 + L3 * cosf(theta3_rad));
+    a.theta2 = RAD2DEG(phi - beta);
 
-
-    a.theta4 = 0.0f;
+    // θ4: orientación deseada del efector (libre)
+    a.theta4 = desired_orientation;
 
     return a;
 }
 
 Velocity differential_kinematics(Angles a, JointVelocity dq) {
-    float t1 = a.theta1 * M_PI / 180.0f;
-    float t2 = a.theta2 * M_PI / 180.0f;
-    float t3 = a.theta3 * M_PI / 180.0f;
+    float t1 = DEG2RAD(a.theta1);
+    float t2 = DEG2RAD(a.theta2);
+    float t3 = DEG2RAD(a.theta3);
 
-    float dt1 = dq.dtheta1 * M_PI / 180.0f;
-    float dt2 = dq.dtheta2 * M_PI / 180.0f;
-    float dt3 = dq.dtheta3 * M_PI / 180.0f;
+    float dt1 = DEG2RAD(dq.dtheta1);
+    float dt2 = DEG2RAD(dq.dtheta2);
+    float dt3 = DEG2RAD(dq.dtheta3);
 
     float s1 = sinf(t1), c1 = cosf(t1);
     float s2 = sinf(t2), c2 = cosf(t2);
     float s23 = sinf(t2 + t3), c23 = cosf(t2 + t3);
 
-    // Derivadas parciales para el Jacobiano J
+    // Derivadas parciales del Jacobiano
     float j11 = -s1 * (L2 * c2 + L3 * c23);
     float j12 = -c1 * (L2 * s2 + L3 * s23);
     float j13 = -c1 * (L3 * s23);
@@ -74,12 +85,11 @@ Velocity differential_kinematics(Angles a, JointVelocity dq) {
     float j32 =  L2 * c2 + L3 * c23;
     float j33 =  L3 * c23;
 
-    // Velocidades lineales
     Velocity v;
     v.dx = j11 * dt1 + j12 * dt2 + j13 * dt3;
     v.dy = j21 * dt1 + j22 * dt2 + j23 * dt3;
     v.dz = j32 * dt2 + j33 * dt3;
+    v.dorientation = dq.dtheta4;  // Independiente
 
     return v;
 }
-
